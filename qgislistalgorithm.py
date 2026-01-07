0f04bfbd08a7750aa05ad0cfbe20f51971bbf45c
Hemed Lungo <86722789+Heed725@users.noreply.github.com>
2026-01-07 11:08:14 +0300

    Update qgislistalgorithm.py

--- qgislistalgorithm.py
+++ qgislistalgorithm.py
@@ -5,3 +5,11 @@ from qgis.core import QgsApplication
 print("=== ALL PROCESSING ALGORITHMS ===\n")
 for alg in QgsApplication.processingRegistry().algorithms():
     print(alg.id())
+
+output_path = "C:/Users/ ... /Desktop/qgis_algorithms.txt"
+
+with open(output_path, 'w') as f:
+    for alg in QgsApplication.processingRegistry().algorithms():
+        f.write(f"{alg.id()} | {alg.displayName()}\n")
+
+print(f"Saved to {output_path}")
